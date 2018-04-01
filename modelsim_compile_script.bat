830169ad3a4aa7a8b2370bedafa7cde47325fa08
Michael Hughes <32088451+contactpro@users.noreply.github.com>
2018-04-01 02:40:51 -0400

    modelsim_compile_script.bat and modelsim_simulate_script.bat
    modelsim_compile_script.bat and modelsim_simulate_script.bat for SVA VHDL BIND Training.

---
+++ modelsim_compile_script.bat
@@ -0,0 +1,39 @@
+REM 
+REM *************************************************************************
+REM
+REM        MODELSIM COMPILE SCRIPT 
+REM 
+REM *************************************************************************
+REM 
+@ECHO OFF
+REM 
+REM *************** Starting modelsim_compile_script.bat ********************
+REM 
+REM Printing scripts Working Directory:
+REM 
+cd
+REM 
+REM Changing Working Directory .....................
+REM 
+cd C:\Users\HP\WORK_MODELSIM\project_one\sim\
+REM 
+REM Printing sim Working Directory:
+REM   
+cd
+REM  
+vlib work
+REM 
+vlog -sv C:\Users\HP\WORK_MODELSIM\project_one\sim\tb\sv_tb_module.sv
+vlog -sv C:\Users\HP\WORK_MODELSIM\project_one\sim\tb\sva_module.sv
+vlog -sv C:\Users\HP\WORK_MODELSIM\project_one\sim\tb\sva_wrapper_module.sv
+REM 
+REM 
+vcom C:\Users\HP\WORK_MODELSIM\project_one\hdl\vhdl\vhdl_dut.vhd
+vcom C:\Users\HP\WORK_MODELSIM\project_one\sim\tb\vhdl_dut_tb.vhd
+REM 
+@ECHO ON
+REM 
+REM 
+REM *************** Completed modelsim_compile_script.bat ******************
+REM 
+REM 
