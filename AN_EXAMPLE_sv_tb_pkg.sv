bfc27ef84ca39cb772413f20c7224238bfc0a86a
Michael Hughes <32088451+contactpro@users.noreply.github.com>
2018-04-15 17:23:30 -0400

    OLD
    OLD

--- AN_EXAMPLE_sv_tb_pkg.sv
+++
@@ -1,9 +0,0 @@
-// file: sv_tb_pkg.sv 
-package sv_tb_pkg;
-  `include "C:/Users/HP/WORK_MODELSIM/project_sv_mem/sim/sv_tb/mem_base_object.svh"
-  `include "C:/Users/HP/WORK_MODELSIM/project_sv_mem/sim/sv_tb/mem_driver.svh"
-  `include "C:/Users/HP/WORK_MODELSIM/project_sv_mem/sim/sv_tb/mem_txgen.svh"
-  `include "C:/Users/HP/WORK_MODELSIM/project_sv_mem/sim/sv_tb/mem_scoreboard.svh"
-  `include "C:/Users/HP/WORK_MODELSIM/project_sv_mem/sim/sv_tb/mem_ip_monitor.svh"
-  `include "C:/Users/HP/WORK_MODELSIM/project_sv_mem/sim/sv_tb/mem_op_monitor.svh"
-endpackage
